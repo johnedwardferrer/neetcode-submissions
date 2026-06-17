b5c8bfefbaa572f2015289cb18a53ce22b2df6e6
johnedwardferrer <johnedwardferrer05@gmail.com>
2026-06-17 18:53:11 +0800

    Add: products-of-array-discluding-self - submission-0

---
+++ Data Structures & Algorithms/products-of-array-discluding-self/submission-0.js
@@ -0,0 +1,22 @@
+class Solution {
+    /**
+     * @param {number[]} nums
+     * @return {number[]}
+     */
+    productExceptSelf(nums) {
+        const result = new Array(nums.length);
+
+        result[0] = 1;
+        for (let i = 1; i < nums.length; i++) {
+            result[i] = result[i - 1] * nums[i - 1];
+        }
+
+        let suffix = 1;
+        for (let i = nums.length - 1; i >= 0; i--) {
+            result[i] *= suffix;
+            suffix *= nums[i];
+        }
+
+        return result;
+    }
+}
